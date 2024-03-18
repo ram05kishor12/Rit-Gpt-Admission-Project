30127955ee91ff53d8e22b96f92d1c25764e4b14
Hackprakas <vijayatrprakash@gmail.com>
2024-03-18 14:15:23 +0530

    loading state added

---
+++ components/loadbtn.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { useFormStatus } from "react-dom"
+import { Button } from "./ui/button";
+
+function Loadingbtn({ content }: { content: string }) {
+    const { pending } = useFormStatus();
+    return (
+        <Button type="submit" disabled={pending} className="text-white w-full  focus:ring-4 focus:outline-none  text-xl rounded-lg  px-5 py-2.5 text-center me-2 mb-2 disabled:bg-opacity-20">
+            {pending ? (<>
+                <div className="flex">
+                    <div className="h-5 w-5 animate-spin rounded-full border-b-2"></div>
+                    <div className="text-white">Submitting...</div>
+                </div>
+            </>
+            ) : (<>{content}</>)}</Button>
+    )
+}
+
+export default Loadingbtn
