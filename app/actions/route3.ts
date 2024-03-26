ac6fbe8bcfdcf26ec8b6de8dea8662753d8995bb
PrakashDev <92098897+Hackprakas@users.noreply.github.com>
2024-03-26 20:58:02 +0530

    Update route3.ts

--- app/actions/route3.ts
+++ app/actions/route3.ts
@@ -3,6 +3,7 @@
 import { Pinecone } from "@pinecone-database/pinecone";
 import {GetEmbeddings}  from "../actions/route1";
 
+
 const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY as string });
 const index = pc.index("chat-pdf");
 const namespace = index.namespace("ns1");
